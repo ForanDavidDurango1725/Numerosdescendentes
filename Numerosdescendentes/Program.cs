ed7e2f882f2558a50aaa7feda6fbfc6654b0496d
ForánDavidDurango1725 <forand.d305@gmail.com>
2023-09-04 23:37:46 -0500

    Agregar archivos de proyecto.

---
+++ Numerosdescendentes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numerosdescendentes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }using System;
+
+class NumerosDescendentes
+    {
+        static void Main()
+        {
+            int i;
+
+            for (i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+}
